d3871d74ce79711af5b4f1e1b1d65246e4498a08
kyusoo Kim <rbtn110@gmail.com>
2022-09-16 14:49:47 +0900

    로그인 요청시 필요한 데이터 username, password 설정

---
+++ src/main/java/com/project/date/dto/request/LoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.project.date.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}

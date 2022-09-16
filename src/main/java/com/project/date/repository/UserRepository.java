03c278d6f650e0846acbf5ca3d40349080c44c05
kyusoo Kim <rbtn110@gmail.com>
2022-09-16 14:37:24 +0900

    UserService에서 이용될 findById, findByUsername 작성

---
+++ src/main/java/com/project/date/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.date.repository;
+
+
+import com.project.date.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
+    Optional<User> findByUsername(String username);
+
+}

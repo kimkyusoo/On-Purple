8f8a01af772ad2798cc8f1d0780645fdfcf8ec8d
kyusoo Kim <rbtn110@gmail.com>
2022-09-16 15:39:17 +0900

    권한(토큰)없이 이용 가능한 api 설정 및 허용, h2-console 허용, CorsConfigurationSource 설정

---
+++ src/main/java/com/project/date/config/SecurityConfiguration.java
@@ -0,0 +1,88 @@
+package com.project.date.config;
+
+import com.project.date.exception.AccessDeniedHandlerException;
+import com.project.date.exception.AuthenticationEntryPointException;
+import com.project.date.impl.UserDetailsServiceImpl;
+import com.project.date.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfiguration {
+
+  @Value("${jwt.secret}")
+  String SECRET_KEY;
+  private final TokenProvider tokenProvider;
+  private final UserDetailsServiceImpl userDetailsService;
+  private final AuthenticationEntryPointException authenticationEntryPointException;
+  private final AccessDeniedHandlerException accessDeniedHandlerException;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  @Order(SecurityProperties.BASIC_AUTH_ORDER)
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.cors();
+    http.headers().frameOptions().disable();
+
+    http.csrf().disable()
+
+        .exceptionHandling()
+        .authenticationEntryPoint(authenticationEntryPointException)
+        .accessDeniedHandler(accessDeniedHandlerException)
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        .and()
+//            권한없이 (=토큰없이) 이용가능한 api설정
+        .authorizeRequests()
+        .antMatchers("/user/**").permitAll()
+        .antMatchers("/post").permitAll()
+        .antMatchers("/comment").permitAll()
+        .antMatchers("/chat").permitAll()
+        .antMatchers("/h2-console/**").permitAll() // h2-console 사용을 위해 추가
+        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // preflight 허용을 위해 추가
+        .anyRequest().authenticated()
+
+        .and()
+        .apply(new JwtSecurityConfiguration(SECRET_KEY, tokenProvider, userDetailsService));
+
+    return http.build();
+  }
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource(){
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedOrigin("http://localhost:3000");
+    configuration.addAllowedHeader("*");
+    configuration.addAllowedMethod("*");
+    configuration.addExposedHeader("*");
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}

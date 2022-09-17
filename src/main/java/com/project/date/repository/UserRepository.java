d536c6984ea0f3f7f80b7ce03daddbb8a0c19fc9
kyusoo Kim <rbtn110@gmail.com>
2022-09-17 16:37:30 +0900

    UserService - creatUser 메소드의 nickname 중복성 검사를 위한 findByNickname 추가

--- src/main/java/com/project/date/repository/UserRepository.java
+++ src/main/java/com/project/date/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long id);
     Optional<User> findByUsername(String username);
 
+    Optional<User> findByNickname(String nickname);
 }

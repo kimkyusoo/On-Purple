9e4513efc07d32aac4b8c1f564a6023e4c4942ae
leeesu <lsu0314@gmail.com>
2022-09-23 08:59:48 +0900

    S3 업로드를 위한 util class2 #2

--- src/main/java/com/project/date/util/AwsS3UploadService.java
+++ src/main/java/com/project/date/util/AwsS3UploadService.java
@@ -5,7 +5,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.project.date.external.dto.S3Component;
+import com.project.date.util.dto.S3Component;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;

517d3750bd95573462bffafe85134f61b172f769
leeesu <lsu0314@gmail.com>
2022-09-25 15:04:15 +0900

    LikeController 구현 createPostLike 생성완료 #11

---
+++ src/main/java/com/project/date/controller/LikeController.java
@@ -0,0 +1,33 @@
+package com.project.date.controller;
+
+import com.project.date.dto.request.PostRequestDto;
+import com.project.date.dto.response.ResponseDto;
+import com.project.date.service.LikeService;
+import com.project.date.service.PostService;
+import com.project.date.util.AwsS3UploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class LikeController {
+
+    private final LikeService likeService;
+
+
+    // 게시글 좋아요
+    @PostMapping( "/post/like/{postId}")
+    public ResponseDto<?> createPostLike(@PathVariable Long postId, HttpServletRequest request) {
+        return likeService.PostLike(postId, request);
+    }
+//    // 댓글 좋아요
+//    @PostMapping( "/comment/like/{commentId}")
+//    public ResponseDto<?> createCommentLike(@PathVariable Long commentId, HttpServletRequest request) {
+//        return likeService.PostLike(commentId, request);
+//    }
+
+}

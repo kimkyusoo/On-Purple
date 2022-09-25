054cfefd3c9a61e64005643b339298718016505a
leeesu <lsu0314@gmail.com>
2022-09-25 17:26:51 +0900

    불필요한 import정리 #12

--- src/main/java/com/project/date/repository/LikeRepository.java
+++ src/main/java/com/project/date/repository/LikeRepository.java
@@ -1,8 +1,6 @@
 package com.project.date.repository;
 
-import com.project.date.model.Comment;
 import com.project.date.model.Likes;
-import com.project.date.model.Post;
 import com.project.date.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 

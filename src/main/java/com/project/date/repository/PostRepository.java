e6676eb7fc4fd65a200ff6e6b6cbf64f204cc739
leeesu <lsu0314@gmail.com>
2022-09-30 10:35:42 +0900

    feat : 카테고리별 필터링, 생성시기로 정렬 #25

--- src/main/java/com/project/date/repository/PostRepository.java
+++ src/main/java/com/project/date/repository/PostRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
   List<Post> findAllByOrderByModifiedAtDesc();
+  List<Post> findAllByCategoryOrderByCreatedAt(String category);
 
 
 }

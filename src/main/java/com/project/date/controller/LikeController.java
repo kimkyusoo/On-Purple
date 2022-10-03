c9319fd7bf36358e222367ae09f265c24a9e1f00
leeesu <lsu0314@gmail.com>
2022-10-04 05:16:47 +0900

    feat : Controller ProfileUnLike 구현 #54

--- src/main/java/com/project/date/controller/LikeController.java
+++ src/main/java/com/project/date/controller/LikeController.java
@@ -23,4 +23,16 @@ public class LikeController {
         return likeService.CommentLike(commentId, request);
     }
 
+    // 댓글 좋아요
+    @PostMapping( "/comment/like/{profileId}")
+    public ResponseDto<?> createProfileLike(@PathVariable Long profileId, HttpServletRequest request) {
+        return likeService.ProfileLike(profileId, request);
+    }
+
+    // 댓글 좋아요
+    @PostMapping( "/comment/like/{profileId}")
+    public ResponseDto<?> createProfileUnLike(@PathVariable Long profileId, HttpServletRequest request) {
+        return likeService.CommentLike(profileId, request);
+    }
+
 }

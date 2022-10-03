674f37d5fe2d5c0cac5691296d89853f75205e0a
leeesu <lsu0314@gmail.com>
2022-10-03 19:28:08 +0900

    feat : import 정리 및 안쓰는 메소드 정리 #40 #26

--- src/main/java/com/project/date/repository/PostRepository.java
+++ src/main/java/com/project/date/repository/PostRepository.java
@@ -1,6 +1,5 @@
 package com.project.date.repository;
 
-import java.util.List;
 
 import com.project.date.model.Post;
 import com.project.date.repository.post.PostCustomRepository;
@@ -9,6 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
-  List<Post> findAllByCategoryOrderByCreatedAtDesc(String category);
 
 }

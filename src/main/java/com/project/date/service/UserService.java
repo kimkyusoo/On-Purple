7fb63784870dacdda5df85941d28af9c45dd1fb9
kyusoo Kim <rbtn110@gmail.com>
2022-10-04 19:40:02 +0900

    프로필 수정 (FE측에서 프로필 작성에 필요한 userId를 조회하지 못하는 문제로 getUser 메소드 구현 및 TokenProvider - payload에 .claim("userId", user.getId())추가

--- src/main/java/com/project/date/service/UserService.java
+++ src/main/java/com/project/date/service/UserService.java
@@ -5,7 +5,6 @@ import com.project.date.dto.response.ResponseDto;
 import com.project.date.dto.response.UserResponseDto;
 import com.project.date.jwt.TokenProvider;
 import com.project.date.model.Img;
-import com.project.date.model.Profile;
 import com.project.date.model.User;
 import com.project.date.repository.ImgRepository;
 import com.project.date.repository.UserRepository;
@@ -29,7 +28,7 @@ public class UserService {
     private final TokenProvider tokenProvider;
     private final ImgRepository imgRepository;
 
-//  회원가입. 유저가 존재하는지, 비밀번호와 비밀번호확인이 일치하는지의 여부를 if문을 통해 확인하고 이를 통과하면 user에 대한 정보를 생성.
+    //  회원가입. 유저가 존재하는지, 비밀번호와 비밀번호확인이 일치하는지의 여부를 if문을 통해 확인하고 이를 통과하면 user에 대한 정보를 생성.
     @Transactional
     public ResponseDto<?> checkUser(String username) {
         Optional<User> user = userRepository.findByUsername(username);
@@ -37,6 +36,7 @@ public class UserService {
             return ResponseDto.fail("DUPLICATED_USERNAME", "중복된 ID 입니다.");
         return ResponseDto.success("사용 가능한 ID입니다.");
     }
+
     @Transactional
     public ResponseDto<?> checkNickname(String nickname) {
         Optional<User> user = userRepository.findByNickname(nickname);
@@ -44,8 +44,9 @@ public class UserService {
             return ResponseDto.fail("DUPLICATED_NICKANAME", "중복된 닉네임 입니다.");
         return ResponseDto.success("사용 가능한 닉네임 입니다.");
     }
+
     @Transactional
-    public ResponseDto<UserResponseDto> createUser(SignupRequestDto requestDto, List<String> imgPaths) {
+    public ResponseDto<UserResponseDto> createUser(SignupRequestDto requestDto, List<String> imgPaths, HttpServletResponse response) {
         if (!requestDto.getPassword().equals(requestDto.getPasswordConfirm())) {
             return ResponseDto.fail("PASSWORDS_NOT_MATCHED",
                     "비밀번호와 비밀번호 확인이 일치하지 않습니다.");
@@ -67,6 +68,9 @@ public class UserService {
         }
         user.imageSave(imgList.get(0));
 
+        TokenDto tokenDto = tokenProvider.generateTokenDto(user);
+        tokenToHeaders(tokenDto, response);
+
         return ResponseDto.success(
                 UserResponseDto.builder()
                         .userId(user.getId())
@@ -79,7 +83,7 @@ public class UserService {
     }
 
     private void postBlankCheck(List<String> imgPaths) {
-        if(imgPaths == null || imgPaths.isEmpty()){ //.isEmpty()도 되는지 확인해보기
+        if (imgPaths == null || imgPaths.isEmpty()) { //.isEmpty()도 되는지 확인해보기
             throw new NullPointerException("이미지를 등록해주세요(Blank Check)");
         }
     }
@@ -111,6 +115,57 @@ public class UserService {
                         .build()
         );
 
+    }
+
+//    @Transactional(readOnly = true)
+//    public ResponseDto<?> getAllUser() {
+//        return ResponseDto.success(userRepository.findAllByOrderByModifiedAtDesc());
+//    }
+
+//    @Transactional
+//    public ResponseDto<?> getAllUsers() {
+//        List<User> userList = userRepository.findAllByOrderByModifiedAtDesc();
+//        List<UserResponseDto> userResponseDto = new ArrayList<>();
+//        for (User user : userList) {
+//            List<Img> findImgList = imgRepository.findByUser_id(user.getId());
+//            List<String> imgList = new ArrayList<>();
+//            for (Img img : findImgList) {
+//                imgList.add(img.getImageUrl());
+//            }
+//            userResponseDto.add(
+//                    UserResponseDto.builder()
+//                            .userId(user.getId())
+//                            .nickname(user.getNickname())
+//                            .imageUrl(imgList.get(0))
+//                            .build()
+//            );
+//        }
+//        return ResponseDto.success(userResponseDto);
+//    }
+
+    @Transactional
+    public ResponseDto<?> getUser(HttpServletRequest request){
+
+        if (null == request.getHeader("RefreshToken")) {
+            return ResponseDto.fail("USER_NOT_FOUND",
+                    "로그인이 필요합니다.");
+        }
+
+        if (null == request.getHeader("Authorization")) {
+            return ResponseDto.fail("USER_NOT_FOUND",
+                    "로그인이 필요합니다.");
+        }
+        User user = validateUser(request);
+        if (null == user) {
+            return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
+        }
+        return ResponseDto.success(
+                UserResponseDto.builder()
+                        .userId(user.getId())
+                        .nickname(user.getNickname())
+                        .imageUrl(user.getImageUrl())
+                        .build()
+        );
     }
 
 //    @Transactional
@@ -174,19 +229,20 @@ public class UserService {
 //        );
 //    }
 
-    //  로그아웃. HttpServletRequest에 있는 권한을 보내 토큰을 확인하여 일치하지 않거나 유저 정보가 없
-    public ResponseDto<?> logout(HttpServletRequest request) {
-        if (!tokenProvider.validateToken(request.getHeader("RefreshToken"))) {
-            return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
-        }
-        User user = tokenProvider.getUserFromAuthentication();
-        if (null == user) {
-            return ResponseDto.fail("USER_NOT_FOUND",
-                    "사용자를 찾을 수 없습니다.");
+        //  로그아웃. HttpServletRequest에 있는 권한을 보내 토큰을 확인하여 일치하지 않거나 유저 정보가 없
+        public ResponseDto<?> logout (HttpServletRequest request){
+            if (!tokenProvider.validateToken(request.getHeader("RefreshToken"))) {
+                return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
+            }
+            User user = tokenProvider.getUserFromAuthentication();
+            if (null == user) {
+                return ResponseDto.fail("USER_NOT_FOUND",
+                        "사용자를 찾을 수 없습니다.");
+            }
+
+            return tokenProvider.deleteRefreshToken(user);
         }
 
-        return tokenProvider.deleteRefreshToken(user);
-    }
 
 //    @Transactional
 //    public User validateUser(HttpServletRequest request) {
@@ -216,6 +272,7 @@ public class UserService {
         }
         return tokenProvider.getUserFromAuthentication();
     }
+
 
 
     //  TokenDto와 HttpServletResponse 응답을 헤더에 보낼 경우

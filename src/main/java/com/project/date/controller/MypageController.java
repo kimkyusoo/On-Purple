cfbff8b19a04422269295f82c613a8fa219df229
kyusoo Kim <rbtn110@gmail.com>
2022-10-06 05:20:05 +0900

    마이페이지 구현 #14

---
+++ src/main/java/com/project/date/controller/MypageController.java
@@ -0,0 +1,40 @@
+package com.project.date.controller;
+
+import com.project.date.dto.response.ResponseDto;
+import com.project.date.model.Profile;
+import com.project.date.service.LikeService;
+import com.project.date.service.MypageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@RestController
+public class MypageController {
+
+    private final MypageService mypageService;
+
+
+
+    @GetMapping("/mypage/{profileId}")
+    public ResponseDto<?> getMypage(HttpServletRequest request, @PathVariable Long profileId){
+
+        return mypageService.getMyPage(request, profileId);
+    }
+
+    //나를 좋아요
+//    @GetMapping("/mypage/like/{profileId}")
+//    public ResponseDto<?> getAllLikeUser(@PathVariable Long profileId, HttpServletRequest request) {
+//        return mypageService.getAllLikeUser(profileId, request);
+//    }
+
+//    @GetMapping("/mypage/tolike/{profileId}")
+//    public ResponseDto<?>toLikeUser(@PathVariable Long profileId, HttpServletRequest request){
+//        return mypageService.toLikeUser(profileId,request);
+//    }
+
+}

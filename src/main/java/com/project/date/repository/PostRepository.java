a1c678fc5b98d3614109d2a7eddb5bd2e7fbc677
leeesu <lsu0314@gmail.com>
2022-10-08 15:37:35 +0900

    refact : 연동문제로 무한스크롤 롤백 + testlist추가 #73

--- src/main/java/com/project/date/repository/PostRepository.java
+++ src/main/java/com/project/date/repository/PostRepository.java
@@ -3,12 +3,18 @@ package com.project.date.repository;
 
 import com.project.date.model.Post;
 import com.project.date.repository.post.PostCustomRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 
-public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
+public interface PostRepository extends JpaRepository<Post, Long>{
+    // , PostCustomRepository
+    List<Post> findAllByCategoryOrderByCreatedAtDesc(String category);
+
+    Page<Post> findAllByCategoryOrderByCreatedAtDesc(String category, Pageable pageable);
 
 //    List<Post> findAllByCategoryOrderByCreatedAtDesc(String category);
 }

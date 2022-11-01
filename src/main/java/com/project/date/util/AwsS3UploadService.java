cfaf2f947a9ca81ba9df5ce5c1a75de56a94de06
leeesu <lsu0314@gmail.com>
2022-11-01 09:48:32 +0900

    feat : 이미지 리사이징 기능 추가 #146

--- src/main/java/com/project/date/util/AwsS3UploadService.java
+++ src/main/java/com/project/date/util/AwsS3UploadService.java
@@ -7,9 +7,17 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.project.date.util.dto.S3Component;
 import lombok.RequiredArgsConstructor;
+import marvin.image.MarvinImage;
+import org.marvinproject.image.transform.scale.Scale;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,11 +38,15 @@ public class AwsS3UploadService implements UploadService {
         // forEach 구문을 통해 multipartFile로 넘어온 파일들 하나씩 fileNameList에 추가
         for (MultipartFile file : multipartFile) {
             String fileName = createFileName(file.getOriginalFilename());
+            String fileFormatName = file.getContentType().substring(file.getContentType().lastIndexOf("/") + 1);
+            MultipartFile resizedFile = resizeImage(fileName, fileFormatName, file, 428);
             ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentLength(resizedFile.getSize());
             objectMetadata.setContentType(file.getContentType());
 
-            try(InputStream inputStream = file.getInputStream()) {
+
+
+            try(InputStream inputStream = resizedFile.getInputStream()) {
                 s3Client.putObject(new PutObjectRequest(component.getBucket(), fileName, inputStream, objectMetadata)
                         .withCannedAcl(CannedAccessControlList.PublicRead));
                 imgUrlList.add(s3Client.getUrl(component.getBucket(), fileName).toString());
@@ -87,4 +99,36 @@ public class AwsS3UploadService implements UploadService {
         return url.substring(url.lastIndexOf('/') + 1, url.length());
     }
 
+    MultipartFile resizeImage(String fileName, String fileFormatName, MultipartFile originalImage, int targetWidth) {
+        try {
+            // MultipartFile로 이미지를 받아와서 BufferedImage로 변환
+            BufferedImage image = ImageIO.read(originalImage.getInputStream());
+            // newWidth : newHeight = originWidth : originHeight
+            int originalWidth = image.getWidth(); //
+            int originalHeight = image.getHeight();
+
+            // origin 이미지가 resizing될 사이즈보다 작을 경우 resizing 작업 안 함
+            if(originalWidth < targetWidth)
+                return originalImage;
+
+            MarvinImage imageMarvin = new MarvinImage(image);
+
+            Scale scale = new Scale();
+            scale.load();
+            scale.setAttribute("newWidth", targetWidth);
+            scale.setAttribute("newHeight", targetWidth * originalHeight / originalWidth);
+            scale.process(imageMarvin.clone(), imageMarvin, null, null, false);
+
+            BufferedImage imageNoAlpha = imageMarvin.getBufferedImageNoAlpha();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ImageIO.write(imageNoAlpha, fileFormatName, byteArrayOutputStream);
+            byteArrayOutputStream.flush();
+
+            return new MockMultipartFile(fileName, byteArrayOutputStream.toByteArray());
+
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 리사이즈에 실패했습니다.");
+        }
+    }
 }
+
